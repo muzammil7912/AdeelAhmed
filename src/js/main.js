8c8faae721614c0f3b0773714204fc06beff7165
Muzammil <muzzammil.warid@gmail.com>
2024-07-13 00:09:26 -0700

    adeelahmed

---
+++ src/js/main.js
@@ -0,0 +1,489 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let scroll;
+
+const body = document.body;
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+//const container = select('.site-main');
+
+initPageTransitions();
+
+// Animation - First Page Load
+function initLoaderHome() { 
+
+  var tl = gsap.timeline();
+
+	tl.set(".loading-screen", { 
+		top: "0",
+	});	
+
+  if ($(window).width() > 540) { 
+    tl.set("main .once-in", {
+      y: "50vh"
+    });
+  } else {
+    tl.set("main .once-in", {
+      y: "10vh"
+    });
+  }
+
+  tl.set(".loading-words", { 
+		opacity: 0,
+    y: -50
+	});
+
+  tl.set(".loading-words .active", { 
+		display: "none",
+	});
+
+  tl.set(".loading-words .home-active, .loading-words .home-active-last", { 
+		display: "block",
+    opacity: 0
+	});
+
+  tl.set(".loading-words .home-active-first", { 
+		opacity: 1,
+	});
+
+  if ($(window).width() > 540) { 
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "10vh",
+    });	
+  } else {
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "5vh",
+    });	
+  }
+
+  tl.set("html", { 
+		cursor: "wait"
+	});
+
+  tl.call(function() {
+    scroll.stop();
+  });
+
+  tl.to(".loading-words", {
+		duration: .8,
+		opacity: 1,
+    y: -50,
+    ease: "Power4.easeOut",
+    delay: .5
+	});
+
+  tl.to(".loading-words .home-active", {
+		duration: .01,
+		opacity: 1,
+    stagger: .15,
+    ease: "none",
+    onStart: homeActive
+  },"=-.4");
+
+  function homeActive() {
+    gsap.to(".loading-words .home-active", {
+      duration: .01,
+      opacity: 0,
+      stagger: .15,
+      ease: "none",
+      delay: .15
+    });
+  }
+
+  tl.to(".loading-words .home-active-last", {
+		duration: .01,
+		opacity: 1,
+    delay: .15
+  });
+  
+	tl.to(".loading-screen", {
+		duration: .8,
+		top: "-100%",
+		ease: "Power4.easeInOut",
+    delay: .2
+  });
+
+  tl.to(".loading-screen .rounded-div-wrap.bottom", {
+		duration: 1,
+		height: "0vh",
+		ease: "Power4.easeInOut"
+	},"=-.8");
+
+  tl.to(".loading-words", {
+		duration: .3,
+		opacity: 0,
+    ease: "linear"
+	},"=-.8");
+
+	tl.set(".loading-screen", { 
+		top: "calc(-100%)" 
+	});	
+
+  tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+		height: "0vh"
+	});	
+
+  tl.to("main .once-in", {
+		duration: 1.5,
+    y: "0vh",
+    stagger: .07,
+		ease: "Expo.easeOut",
+    clearProps: true
+	},"=-.8");
+
+  tl.set("html", { 
+		cursor: "auto"
+	},"=-1.2");
+
+  tl.call(function() {
+    scroll.start();
+  });
+  
+}
+
+// Animation - First Page Load
+function initLoader() { 
+
+  var tl = gsap.timeline();
+
+	tl.set(".loading-screen", { 
+		top: "0",
+	});	
+
+  if ($(window).width() > 540) { 
+    tl.set("main .once-in", {
+      y: "50vh"
+    });
+  } else {
+    tl.set("main .once-in", {
+      y: "10vh"
+    });
+  }
+
+  tl.set(".loading-words", { 
+		opacity: 1,
+    y: -50
+	});
+
+  if ($(window).width() > 540) { 
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "10vh",
+    });	
+  } else {
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "5vh",
+    });	
+  }
+
+  tl.set("html", { 
+		cursor: "wait"
+	});
+  
+	tl.to(".loading-screen", {
+		duration: .8,
+		top: "-100%",
+		ease: "Power4.easeInOut",
+    delay: .5
+  });
+
+  tl.to(".loading-screen .rounded-div-wrap.bottom", {
+		duration: 1,
+		height: "0vh",
+		ease: "Power4.easeInOut"
+	},"=-.8");
+
+  tl.to(".loading-words", {
+		duration: .3,
+		opacity: 0,
+    ease: "linear",
+	},"=-.8");
+
+	tl.set(".loading-screen", { 
+		top: "calc(-100%)" 
+	});	
+
+  tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+		height: "0vh"
+	});	
+
+  tl.to("main .once-in", {
+		duration: 1,
+    y: "0vh",
+    stagger: .05,
+		ease: "Expo.easeOut",
+    clearProps: "true"
+	},"=-.8");
+
+  tl.set("html", { 
+		cursor: "auto",
+	},"=-.8");
+
+}
+
+
+// Animation - Page transition In
+function pageTransitionIn() {
+	var tl = gsap.timeline();
+
+  tl.call(function() {
+    scroll.stop();
+  });
+
+  tl.set(".loading-screen", { 
+		top: "100%" 
+	});	
+
+  tl.set(".loading-words", { 
+		opacity: 0,
+    y: 0
+	});
+
+  tl.set("html", { 
+		cursor: "wait"
+	});
+
+  if ($(window).width() > 540) { 
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "10vh",
+    });	
+  } else {
+    tl.set(".loading-screen .rounded-div-wrap.bottom", { 
+      height: "5vh",
+    });	
+  }
+
+	tl.to(".loading-screen", {
+		duration: .5,
+		top: "0%",
+		ease: "Power4.easeIn"
+	});
+
+  if ($(window).width() > 540) { 
+    tl.to(".loading-screen .rounded-div-wrap.top", {
+      duration: .4,
+      height: "10vh",
+      ease: "Power4.easeIn"
+    },"=-.5");
+  } else {
+    tl.to(".loading-screen .rounded-div-wrap.top", {
+      duration: .4,
+      height: "10vh",
+      ease: "Power4.easeIn"
+    },"=-.5");
+  }
+
+  tl.to(".loading-words", {
+		duration: .8,
+		opacity: 1,
+    y: -50,
+    ease: "Power4.easeOut",
+    delay: .05
+	});
+
+  tl.set(".loading-screen .rounded-div-wrap.top", {
+		height: "0vh"
+	});
+
+	tl.to(".loading-screen", {
+		duration: .8,
+		top: "-100%",
+		ease: "Power3.easeInOut"
+	},"=-.2");
+
+  tl.to(".loading-words", {
+		duration: .6,
+		opacity: 0,
+    ease: "linear"
+	},"=-.8");
+
+  tl.to(".loading-screen .rounded-div-wrap.bottom", {
+		duration: .85,
+		height: "0",
+		ease: "Power3.easeInOut"
+	},"=-.6");
+
+  tl.set("html", { 
+		cursor: "auto"
+	},"=-0.6");
+
+  if ($(window).width() > 540) { 
+    tl.set(".loading-screen .rounded-div-wrap.bottom", {
+      height: "10vh"
+    });
+  } else {
+    tl.set(".loading-screen .rounded-div-wrap.bottom", {
+      height: "5vh"
+    });
+  }
+
+  tl.set(".loading-screen", { 
+		top: "100%" 
+	});	
+
+  tl.set(".loading-words", {
+		opacity: 0,
+	});
+  
+}
+
+
+// Animation - Page transition Out
+function pageTransitionOut() {
+	var tl = gsap.timeline();
+
+  if ($(window).width() > 540) { 
+    tl.set("main .once-in", {
+      y: "50vh",
+    });
+  } else {
+    tl.set("main .once-in", {
+      y: "20vh"
+    });
+  }
+  
+  tl.call(function() {
+    scroll.start();
+  });
+
+  tl.to("main .once-in", {
+    duration: 1,
+    y: "0vh",
+    stagger: .05,
+    ease: "Expo.easeOut",
+    delay: .8,
+    clearProps: "true"
+  });
+
+}
+
+function initPageTransitions() {
+
+  //let scroll;
+
+  // do something before the transition starts
+  barba.hooks.before(() => {
+    select('html').classList.add('is-transitioning');
+  });
+
+  // do something after the transition finishes
+  barba.hooks.after(() => {
+    select('html').classList.remove('is-transitioning');
+    // reinit locomotive scroll
+    scroll.init();
+    scroll.stop();
+  });
+
+  // scroll to the top of the page
+  barba.hooks.enter(() => {
+    scroll.destroy();
+  });
+
+  // scroll to the top of the page
+  barba.hooks.afterEnter(() => {
+    window.scrollTo(0, 0);
+    initCookieViews();
+  });
+
+  if ($(window).width() > 540) { 
+    barba.hooks.leave(() => {
+      $(".btn-hamburger, .btn-menu").removeClass('active');
+      $("main").removeClass('nav-active');
+    }); 
+  }
+  
+
+  barba.init({
+    sync: true,
+    debug: false,
+    timeout:7000,
+    transitions: [{
+      name: 'default',
+      once(data) {
+        // do something once on the initial page load
+        initSmoothScroll(data.next.container);
+        initScript();
+        initCookieViews();
+        initLoader();
+      },
+      async leave(data) {
+        // animate loading screen in
+        pageTransitionIn(data.current);
+        await delay(495);
+        data.current.container.remove();
+      },
+      async enter(data) {
+        // animate loading screen away
+        pageTransitionOut(data.next);
+        initNextWord(data);
+      },
+      async beforeEnter(data) {
+        ScrollTrigger.getAll().forEach(t => t.kill());
+        scroll.destroy();
+        initSmoothScroll(data.next.container);
+        initScript(); 
+      },
+    }, 
+    {
+      name: 'to-home',
+      from: {
+      },
+      to: {
+        namespace: ['home']
+      },
+      once(data) {
+        // do something once on the initial page load
+        initSmoothScroll(data.next.container);
+        initScript();
+        initCookieViews();
+        initLoaderHome();
+      },
+    }]
+  });
+
+  function initSmoothScroll(container) {
+
+    scroll = new LocomotiveScroll({
+      el: container.querySelector('[data-scroll-container]'),
+      smooth: true,
+    });
+
+    window.onresize = scroll.update();
+
+    scroll.on("scroll", () => ScrollTrigger.update());
+
+    ScrollTrigger.scrollerProxy('[data-scroll-container]', {
+      scrollTop(value) {
+        return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: container.querySelector('[data-scroll-container]').style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.defaults({
+      scroller: document.querySelector('[data-scroll-container]'),
+    });
+
+    /**
+     * Remove Old Locomotive Scrollbar
+     */
+
+    const scrollbar = selectAll('.c-scrollbar');
+
+    if(scrollbar.length > 1) {
+      scrollbar[0].remove();
+    }
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener('refresh', () => scroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+  }  
+}
+
+
+
+
